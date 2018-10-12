3f6be39f045b99b717dca7507f6227a71cf111c4
wangpeng <wangpeng@ziipin.com>
2018-10-12 17:21:34 +0800

    更新球最大子序列和

---
+++ code0x00E.go
@@ -0,0 +1,26 @@
+package main
+
+/*
+ * 最大子序和
+ * https://leetcode-cn.com/problems/maximum-subarray/
+ */
+
+func maxSubArray(nums []int) int {
+	var sum = nums[0]
+	var n = nums[0]
+	for i := 1; i < len(nums); i++ {
+		if n > 0 {
+			n += nums[i]
+		} else {
+			n = nums[i]
+		}
+		if sum < n {
+			sum = n
+		}
+	}
+	return sum
+}
+
+func MaxSubArray(nums []int) int {
+	return maxSubArray(nums)
+}
---
+++ code0x00E_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func TestMaxSubArray(t *testing.T) {
+	t.Log(MaxSubArray([]int{-2,1,-3,4,-1,2,1,-5,4}))
+}
