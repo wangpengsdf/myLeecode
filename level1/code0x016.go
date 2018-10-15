5aa98970a0726c2d23a8cb3ad56209d4690fb0df
wangpeng <wangpeng@ziipin.com>
2018-10-15 15:40:47 +0800

    更新树相等判断

---
+++ level1/code0x016.go
@@ -0,0 +1,25 @@
+package main
+
+/*
+ * 相同的树
+ * https://leetcode-cn.com/problems/same-tree/
+ */
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil{return true}
+	if p != nil && q == nil{return false}
+	if p == nil && q != nil{return false}
+	if p.Val != q.Val{return false}
+	return isSameTree(p.Left,q.Left) && isSameTree(p.Right,q.Right)
+}
+
+func IsSameTree(p *TreeNode, q *TreeNode) bool {
+	return isSameTree(p,q)
+}
+
