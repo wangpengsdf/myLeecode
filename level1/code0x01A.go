e8263e53459bdaf14af81286db111cf5c446dc77
wangpeng <wangpeng@ziipin.com>
2018-10-15 17:13:26 +0800

    将有序数组转换为二叉搜索树

---
+++ level1/code0x01A.go
@@ -0,0 +1,21 @@
+package main
+
+/*
+ * 将有序数组转换为二叉搜索树
+ * https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree
+ */
+
+
+ func sortedArrayToBST(nums []int) *TreeNode {
+ 	if len(nums) == 0{
+ 		return nil
+	}
+	var center = len(nums)/2
+	root := new(TreeNode)
+	root.Val = nums[center]
+	root.Left = sortedArrayToBST(nums[:center])
+	root.Right = sortedArrayToBST(nums[center+1:])
+	return root
+ }
+
+
