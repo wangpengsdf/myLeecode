f02e3b85e1763b6c101396f0d0e9e435a78f7752
wangpeng <wangpeng@ziipin.com>
2018-10-15 17:59:08 +0800

    更新路径综合计算

---
+++ level1/code0x01D.go
@@ -0,0 +1,18 @@
+package main
+
+/*
+ * 路径总和
+ * https://leetcode-cn.com/problems/path-sum/
+ */
+
+
+func hasPathSum(root *TreeNode, sum int) bool {
+	if root == nil {
+		return false
+	}
+	extra := sum - root.Val
+	if extra == 0 && root.Left == nil && root.Right == nil{
+		return true
+	}
+	return hasPathSum(root.Left,extra) ||  hasPathSum(root.Right,extra)
+}
