a1a5b05dbd378f029190e6564555b83d2c0e72c4
wangpeng <wangpeng@ziipin.com>
2018-10-15 16:33:40 +0800

    更新二叉树镜像判断

---
+++ level1/code0x018.go
@@ -0,0 +1,18 @@
+package main
+
+/*
+ * 二叉树深度判断
+ * https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+ */
+func maxDepth(root *TreeNode) int {
+	if root == nil{
+		return 0
+	}
+	left := maxDepth(root.Left)
+	right := maxDepth(root.Right)
+	if left > right{
+		return left + 1
+	}else{
+		return right + 1
+	}
+}
